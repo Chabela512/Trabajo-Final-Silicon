59ef7802317658cb21096291dcdbba8f7d7fe2aa
Chabela <chabelagonzale512@gmail.com>
2023-12-08 20:09:40 -0300

    Final

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', mostrarCarrito);
-        // Función para mostrar los producto del carrito en la página del carrito
+        
         function mostrarCarrito() {
             let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
             let listaCarrito = document.getElementById('lista-carrito');
@@ -8,8 +8,7 @@ document.addEventListener('DOMContentLoaded', mostrarCarrito);
             carrito.forEach((producto, index) => {
                 let li = document.createElement('li');
                 li.textContent = `${producto.nombre} - $${producto.precio}`;
-                
-                // Botón para borrar el producto del carrito
+       
                 let botonBorrar = document.createElement('button');
                 botonBorrar.textContent = 'Borrar';
                 botonBorrar.addEventListener('click', () => {
@@ -29,10 +28,10 @@ document.addEventListener('DOMContentLoaded', mostrarCarrito);
             mostrarCarrito();
         }
 
-        // Función para completar la compra y variar el carrito
+     
         function completarCompra(){
-            //pueden agregar cualquier lógica adicional que deseen para procesar la compra
+       
             alert('¡Compra completada con éxito! Gracias por su compra.');
-            localStorage.removeItem('carrito'); // Vaciar el carrito
-            mostrarCarrito(); // Actualizar la lista del carrito (se mostrará vacía)
+            localStorage.removeItem('carrito'); 
+            mostrarCarrito();
         }
